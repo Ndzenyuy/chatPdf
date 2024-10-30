7490e6a18502d1985e8623423cfd4f761f400886
Ndzenyuy <ndzenyuyjones@gmail.com>
2024-10-30 23:02:13 +0100

    pdf chatbot init

---
+++ Admin/admin.py
@@ -0,0 +1,36 @@
+import boto3
+import streamlit as st
+import os
+import uuid
+
+## s3_clinet
+s3_client = boto3.client("s3")
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+
+## Amazon Bedrock
+from langchain_community.embeddings import BedrockEmbeddings
+
+## Text Splitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+## Pdf Loader
+from langchain_community.document_loaders import PyPDFLoader
+
+def get_unique_id():
+    return str(uuid.uuid4())
+def main():
+    st.write("This is admin site for chat with PDF")
+    if uploaded_file is not None:
+        request_id = get_unique_id()
+        st.write(f"Request Id: {request_id}")
+        saved_file_name = f"{request_id}.pdf"
+        with open(saved_file_name, mode="wb") as w:
+            w.write(uploaded_file.getvalue())
+        
+        loader = PyPDFLoader(saved_file_name)
+        pages = loader.load_and_split()
+
+        st.write(f"Total Pages: {len(pages)}")
+
+if __name__ == "__main__":
+    main()
