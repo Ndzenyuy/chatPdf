1fdbee3b72f55e6e21b54e4c2241a1c168d02633
Ndzenyuy <ndzenyuyjones@gmail.com>
2024-10-31 11:25:34 +0100

    completed client side

--- User/app.py
+++ User/app.py
@@ -9,6 +9,10 @@ BUCKET_NAME = os.getenv("BUCKET_NAME")
 
 ## Bedrock
 from langchain_community.embeddings import BedrockEmbeddings
+from langchain.llms.bedrock import Bedrock
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
 
 ## Text Splitter
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -32,8 +36,46 @@ folder_path="/tmp/"
 
 ## Load index
 def load_index():
-    s3_client.download_file(Bucket=BUCKET_NAME, Key="my_faiss.faiss", Filename=f"{folder_path}myfaiss.faiss")
-    s3_client.download_file(Bucket=BUCKET_NAME, Key="my_faiss.pkl", Filename=f"{folder_path}myfaiss.pkl")
+    s3_client.download_file(Bucket=BUCKET_NAME, Key="my_faiss.faiss", Filename=f"{folder_path}my_faiss.faiss")
+    s3_client.download_file(Bucket=BUCKET_NAME, Key="my_faiss.pkl", Filename=f"{folder_path}my_faiss.pkl")
+
+def get_llm():
+    llm=Bedrock(model_id="anthropic.claude-v2:1", client=bedrock_client,
+                model_kwargs={'max_tokens_to_sample':512})
+    return llm
+
+## get response
+def get_response(llm,vectorstore, question ):
+    ## create prompt / template
+    prompt_template = """
+
+    Human: Please use the given context to provide concise answer to the question
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    <context>
+    {context}
+    </context>
+
+    Question: {question}
+
+    Assistant:"""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore.as_retriever(
+        search_type="similarity", search_kwargs={"k": 5}
+    ),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": PROMPT}
+)
+    answer=qa({"query":question})
+    return answer['result']
+
+
 
 ## Main method
 def main():
@@ -42,6 +84,27 @@ def main():
     dir_list = os.listdir(folder_path)
     st.write(f"Files and Directories in {folder_path}")
     st.write(dir_list)
+
+    ## Create index
+    faiss_index = FAISS.load_local(
+        index_name="my_faiss"
+        folder_path = folder_path
+        embeddings=bedrock_embeddings,
+        allow_dangerous_deserialization=True
+    )
+
+    st.write("INDEX IS READY")
+    question = st.text_input("Please ask your question")
+
+    if st.button("Ask Question"):
+        with st.spinner("Querying...")
+        llm = get_llm()
+
+        # get_response
+        st.write(get_response(llm, faiss_index, question))
+        st.success("Done")
+
+
 
 if __name__ == "__main__":
     main()
